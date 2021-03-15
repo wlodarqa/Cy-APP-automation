3d987e1da1a092b4118354eef08aff3d1bc139f0
WlodarQA <k.wlodarek@p-programisci.pl>
2021-03-15 16:18:08 +0100

    intercept add

--- cypress/integration/tests/login.spec.js
+++ cypress/integration/tests/login.spec.js
@@ -1,19 +1,21 @@
 describe('Login tests ', () => {
     beforeEach(() => {
         cy.visit('/')
+        cy.intercept('POST', '/login').as('login');
         
     })
 
     it('go to login page', () => {
         cy.url().should('include', '/signin')
         cy.contains('Sign in').should('be.visible')
-        cy.intercept('POST', '/login').as('login');
+        
     })
     it('login with valid date', () => {
         cy.get('#username').type('wlodarqa')
         cy.get('#password').type('test')
         cy.get('[type="checkbox"]').check()
         cy.get('.MuiButton-label').click()
+        cy.get('.makeStyles-root-1').should('be.visible').as('homepage')
         
      })
     })
