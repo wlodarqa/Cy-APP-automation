6108085a671088c20714632cce6484625033d3f0
WlodarQA <k.wlodarek@p-programisci.pl>
2021-03-16 12:15:34 +0100

    add tests with invalid data and without checkbox

--- cypress/integration/tests/login.spec.js
+++ cypress/integration/tests/login.spec.js
@@ -1,21 +1,35 @@
 describe('Login tests ', () => {
     beforeEach(() => {
         cy.visit('/')
-        cy.intercept('POST', '/login').as('login');
-        
-    })
+        cy.intercept('POST', '/login').as('login');   
+        })
 
     it('go to login page', () => {
         cy.url().should('include', '/signin')
         cy.contains('Sign in').should('be.visible')
-        
-    })
-    it('login with valid date', () => {
+        })
+
+    it('login with valid data', () => {
         cy.get('#username').type('wlodarqa')
         cy.get('#password').type('test')
         cy.get('[type="checkbox"]').check()
         cy.get('.MuiButton-label').click()
-        cy.get('.makeStyles-root-1').should('be.visible').as('homepage')
-        
-     })
+        cy.get('.makeStyles-root-1').should('be.visible')
+        })
+
+    it('login with invalid data', () => {
+        cy.get('#username').type('wrong name')
+        cy.get('#password').type('wrong password')
+        cy.get('[type="checkbox"]').check()
+        cy.get('.MuiButton-label').click()
+        cy.get('.MuiAlert-message').should('be.visible')
+        })
+
+        it('login without checbox', () => {
+            cy.get('#username').type('wlodarqa')
+            cy.get('#password').type('test')
+            cy.get('.MuiButton-label').click()
+            cy.wait(2000)
+            cy.get('.makeStyles-root-1').should('not.be.visible')
+            })
     })
