c73fc2d9fd39eb6c6166d3b2fd49dc99bcf92768
WlodarQA <k.wlodarek@p-programisci.pl>
2021-04-17 18:03:30 +0200

    Login with custom commands

--- cypress/integration/tests/login.spec.js
+++ cypress/integration/tests/login.spec.js
@@ -1,35 +1,31 @@
-describe('Login tests ', () => {
+describe('Login tests', () => {
     beforeEach(() => {
         cy.visit('/')
-        cy.intercept('POST', '/login').as('login');   
+            .intercept('POST', '/login').as('login');   
         })
-
     it('go to login page', () => {
         cy.url().should('include', '/signin')
+        cy.title().should('eq', 'Cypress Real World App')
         cy.contains('Sign in').should('be.visible')
         })
-
-    it('login with valid data', () => {
-        cy.get('#username').type('wlodarqa')
-        cy.get('#password').type('test')
-        cy.get('[type="checkbox"]').check()
-        cy.get('.MuiButton-label').click()
-        cy.get('.makeStyles-root-1').should('be.visible')
+     it('login with invalid data', () => {
+        cy.invalid_login('wrong_username', 'wrong_password')
+            .wait('@login').then(intercept => {
+            cy.log(intercept)
         })
-
-    it('login with invalid data', () => {
-        cy.get('#username').type('wrong name')
-        cy.get('#password').type('wrong password')
-        cy.get('[type="checkbox"]').check()
-        cy.get('.MuiButton-label').click()
-        cy.get('.MuiAlert-message').should('be.visible')
-        })
-
-        it('login without checkbox', () => {
-            cy.get('#username').type('wlodarqa')
-            cy.get('#password').type('test')
-            cy.get('.MuiButton-label').click()
-            cy.wait(2000)
-            cy.get('.makeStyles-root-1').should('not.be.visible')
             })
+    it('login with valid data', () => {
+        cy.login('username', 'password')
+            .wait('@login').then(intercept => {
+            cy.log(intercept)
+        })
+            .get('.makeStyles-root-1').should('be.visible')
+        })
+    it('login without checkbox', () => {
+        cy.login_checkbox('username', 'password')
+            .wait('@login').then(intercept => {
+            cy.log(intercept)
+        })
+        .get('.makeStyles-root-1').should('be.visible')
+        })
     })
