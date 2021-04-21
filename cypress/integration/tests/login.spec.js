18d9324637f22933e8596c3b3cbb41854a555358
WlodarQA <k.wlodarek@p-programisci.pl>
2021-04-21 13:46:44 +0200

    new login flow

--- cypress/integration/tests/login.spec.js
+++ cypress/integration/tests/login.spec.js
@@ -1,39 +1,42 @@
 describe('Login tests', () => {
     beforeEach(() => {
-        cy.visit('/')
-        cy.clearCookies()
-        cy.clearLocalStorage()
-        cy.url().should('include', '/signin')
-        cy.title().should('eq', 'Cypress Real World App')
-        cy.contains('Sign in').should('be.visible')
+        cy.visit('/');
+        cy.clearCookies();
+        cy.clearLocalStorage();
+        cy.url().should('include', '/signin');
+        cy.title().should('eq', 'Cypress Real World App');
+        cy.contains('Sign in').should('be.visible');
         cy.intercept('POST', '/login').as('apiLogin');   
     })
 
     it('login with invalid data', () => {
-        cy.invalidLogin('wrong_username', 'wrong_password')
+        cy.login('wrong_username', 'wrong_password');
         cy.wait('@apiLogin').then(intercept => {
-            cy.log(intercept.request.body)
+            cy.log(intercept.request.body);
+            cy.log(intercept.response.body);
+            expect(intercept.response.statusCode).to.equal (401);
         })
+        cy.get('.MuiAlert-message').should('be.visible')
     })
 
     it('login with valid data', () => {
-        cy.login('username', 'password')
-        cy.get('.MuiButton-label').click()
-        cy.wait('@apiLogin', ).then(intercept => {
-            cy.log(intercept.request.body)
+        cy.login('wlodarqa', 'test');
+        cy.wait('@apiLogin').then(intercept => {
+            cy.log(intercept.request.body);
+            expect(intercept.request.body.remember).to.not.exist
         })
-        cy.get('.makeStyles-root-1').should('be.visible')
+        cy.get('.makeStyles-root-1').should('be.visible');
     })
 
-    it('login with checkbox', () => {
-        cy.login('username', 'password')
-        cy.get('[type="checkbox"]').check()
-        cy.get('.MuiButton-label').click()     
+    it('login with valid data and checkbox', () => {
+        cy.login('wlodarqa', 'test', true);   
         cy.wait('@apiLogin').then(intercept => {
-            cy.log(intercept)
-            cy.log(intercept.request.body.remember).as('checbox')
-                expect(intercept.request.body.remember).to.be.true
+            cy.log(intercept);
+            cy.log(intercept.request.body.remember).as('remember');
+            expect(intercept.request.body.remember).to.exist;
+            expect(intercept.request.body.remember).to.be.true;
+
        })
-        cy.get('.makeStyles-root-1').should('be.visible')
+        cy.get('.makeStyles-root-1').should('be.visible');
     })
 })
